c577e1462ea21d1b88c709de47bcd37a3ccada86
ThoaiNguyen <thoaikhmt@gmail.com>
2019-08-22 22:18:15 +0700

    implement placement driver

--- gen-proto.sh
+++ gen-proto.sh
@@ -9,3 +9,5 @@ protoc -I/usr/local/include -I. \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --grpc-gateway_out=logtostderr=true:./pkg \
   ./api/raftkv/raftkv.proto
+
+
