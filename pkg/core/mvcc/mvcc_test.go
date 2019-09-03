b5e099a100a5b0896194ca47e18d2ca4642d37ae
ThoaiNguyen <thoaikhmt@gmail.com>
2019-09-03 17:25:36 +0700

    update mvcc

--- pkg/core/mvcc/mvcc_test.go
+++ pkg/core/mvcc/mvcc_test.go
@@ -2,22 +2,21 @@ package mvcc
 
 import (
 	"bytes"
-	"testing"
 	"encoding/binary"
 	"log"
+	"testing"
+
 	pb "github.com/thoainguyen/mtikv/pkg/pb/mtikvpb"
 )
 
-
-
-func TestPrewrite (t *testing.T){
+func TestPrewrite(t *testing.T) {
 	m := CreateMvcc("volumes")
 	defer m.Destroy()
 
 	mutations := []pb.Mutation{
 		pb.Mutation{
-			Key:   []byte("thuyenpt"),
-			Value: []byte("Phan Trong Thuyen"),
+			Key:   []byte("thoainh"),
+			Value: []byte("Nguyen Huynh Thoai"),
 			Op:    pb.Op_PUT,
 		},
 	}
@@ -28,22 +27,21 @@ func TestPrewrite (t *testing.T){
 	op := make([]byte, 4)
 	binary.BigEndian.PutUint32(op, uint32(pb.Op_PUT))
 
-	_, errPrewrite := m.Prewrite(mutations, 1, []byte("thuyenpt"))
+	_, errPrewrite := m.Prewrite(mutations, 1, []byte("thoainh"))
 	if errPrewrite != nil {
 		log.Fatal(errPrewrite)
 	}
 
-	data := m.GetStore().Get(0, bytes.Join([][]byte{[]byte("thuyenpt"), startTs}, []byte("|")))
-	if (bytes.Compare(data, []byte("Phan Trong Thuyen")) != 0) {
+	data := m.GetStore().Get(0, bytes.Join([][]byte{[]byte("thoainh"), startTs}, []byte("|")))
+	if bytes.Compare(data, []byte("Nguyen Huynh Thoai")) != 0 {
 		t.Errorf("CF_DATA isn't writen")
 	}
 
-	lock := m.GetStore().Get(1, []byte("thuyenpt"))
-	if (bytes.Compare(lock,  bytes.Join([][]byte{op, []byte("thuyenpt"), startTs}, []byte("|"))) != 0) {
+	lock := m.GetStore().Get(1, []byte("thoainh"))
+	if bytes.Compare(lock, bytes.Join([][]byte{op, []byte("thoainh"), startTs}, []byte("|"))) != 0 {
 		t.Errorf("CF_LOCK isn't writen")
 	}
 }
-
 
 func TestCommit(t *testing.T) {
 	m := CreateMvcc("volumes")
@@ -51,8 +49,8 @@ func TestCommit(t *testing.T) {
 
 	mutations := []pb.Mutation{
 		pb.Mutation{
-			Key:   []byte("thuyenpt"),
-			Value: []byte("Phan Trong Thuyen"),
+			Key:   []byte("thoainh"),
+			Value: []byte("Nguyen Huynh Thoai"),
 			Op:    pb.Op_PUT,
 		},
 	}
@@ -66,7 +64,7 @@ func TestCommit(t *testing.T) {
 	op := make([]byte, 4)
 	binary.BigEndian.PutUint32(op, uint32(pb.Op_PUT))
 
-	_, errPrewrite := m.Prewrite(mutations, 1, []byte("thuyenpt"))
+	_, errPrewrite := m.Prewrite(mutations, 1, []byte("thoainh"))
 	if errPrewrite != nil {
 		log.Fatal(errPrewrite)
 	}
@@ -76,19 +74,18 @@ func TestCommit(t *testing.T) {
 		log.Fatal(errCommit)
 	}
 
-	write := m.GetStore().Get(2, bytes.Join([][]byte{[]byte("thuyenpt"), commitTs}, []byte("|")))
-	if (bytes.Compare(write, bytes.Join([][]byte{op, startTs}, []byte("|"))) != 0) {
+	write := m.GetStore().Get(2, bytes.Join([][]byte{[]byte("thoainh"), commitTs}, []byte("|")))
+	if bytes.Compare(write, bytes.Join([][]byte{op, startTs}, []byte("|"))) != 0 {
 		t.Errorf("CF_WRITE isn't writen")
 	}
 
-	lock := m.GetStore().Get(1, []byte("thuyenpt"))
-	if (bytes.Compare(lock,  []byte(nil)) != 0) {
+	lock := m.GetStore().Get(1, []byte("thoainh"))
+	if bytes.Compare(lock, []byte(nil)) != 0 {
 		t.Errorf("CF_LOCK is still reserved")
 	}
 
-
-	info := m.GetStore().Get(3, []byte("thuyenpt"))
-	if (bytes.Compare(info,  commitTs) != 0) {
+	info := m.GetStore().Get(3, []byte("thoainh"))
+	if bytes.Compare(info, commitTs) != 0 {
 		t.Errorf("Latest commit in CF_INFO isn't correct")
 	}
 }
